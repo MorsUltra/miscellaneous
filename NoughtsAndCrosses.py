fd47da70c544d187cc583a0ad981d66626886de4
olive <oliverpaynterjones@gmail.com>
2021-11-18 00:37:29 +0000

    NoughtsAndCrosses.py added

--- NoughtsAndCrosses.py
+++ NoughtsAndCrosses.py
@@ -1,42 +1,63 @@
-import itertools
+import random
+
+grid = [
+    [0, 1, 2],  # integers
+    [3, 4, 5],
+    [6, 7, 8],
+]
 
 
-def show_grid(grid):
+# | 0 | 1 | 2 |
+# | 3 | 4 | 5 |
+# | 6 | 7 | 8 |
+
+def print_grid(grid):
     for row in grid:
         print(f"| {row[0]} | {row[1]} | {row[2]} |")
-
-    print("\n\n\n")
-
-
-def move(player):
-    player += 1
-    return player % 2
 
 
 def check_win(grid):
     pass
+#testing
 
+win_conditions = [
+    [[True, True, True],
+     [False, False, False],
+     [False, False, False]]
+    ,
+    [[False, False, False],
+     [True, True, True],
+     [False, False, False]]
+    ,
+    [[False, False, False],
+     [False, False, False],
+     [True, True, True]]
+]
+win = False
+player = 0
+piece = None
 
-grid = [list(range(row * 3, row * 3 + 3)) for row in range(3)]
+while not win:
+    print_grid(grid)
+    # Change the player
+    player += 1
+    player %= 2
+    # Set the piece
+    if player == 1:
+        piece = "x"
+    else:
+        piece = "o"
 
-perms = map(list, set(itertools.permutations([False, False, True])))
+    move_position = int(input("Where do you want to put your piece? "))
+    row = move_position // 3
+    column = move_position % 3
+    grid[row][column] = piece  # check if this is already filled
 
-templates = [[line] * 3 for line in perms] + \
-            [[[line] * 3 for line in combination] for combination in perms]
+    # | 0 | 1 | 2 |
+    # | 3 | 4 | 5 |
+    # | 6 | 7 | 8 |
+    # grid[value] = new_value
 
-player = move(0)
-pos = 0
-
-while not check_win(grid):
-    show_grid(grid)
-    player = move(player)
-    piece = "x" if player else "o"
-
-    pos = int(input(f"Player:{player}\nWhere do you want to put your piece"))
-
-    while not isinstance(grid[pos // 3][pos % 3], int):
-        pos = int(input(f"That's an invalid move\nPlayer:{player}\nWhere do you want to put your piece"))
-
-    grid[pos // 3][pos % 3] = piece
-
-# is this working? Can you type and see shit?
+    # put piece into grid here:
+    # row //
+    # column %
